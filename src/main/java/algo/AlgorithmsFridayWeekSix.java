ca1fe2c93107f600930831b48e7b97181d6802bb
masterscode <ogbinakaemmanuel@gmail.com>
2021-05-15 23:01:27 +0100

    hold up..

--- src/main/java/algo/AlgorithmsFridayWeekSix.java
+++ src/main/java/algo/AlgorithmsFridayWeekSix.java
@@ -1,3 +1,9 @@
+package algo;
+
+import java.util.ArrayList;
+
 class AlgorithmsFridayWeekSix{
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> line = new ArrayList<>();
+    }
 }
